1e7e48f1fdfa4d745605ddf20d68d8021be2da78
Matheus Olimpio de Oliveira <mah.oliver_crz@hotmail.com>
2021-04-27 21:39:54 -0300

    Add page product list and product search

---
+++ src/components/ProdutosLista.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="produtos-container">
+    <div v-for="produto in produtos" :key="produto.id">
+      <img
+        v-if="produto.fotos"
+        :src="produto.fotos[0].src"
+        :alt="produto.fotos[0].titulo"
+      />
+      <h2 class="titulo">{{ produto.nome }}</h2>
+      <p class="preco">{{ produto.preco }}</p>
+      <p class="descricao">{{ produto.descricao }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      produtos: null,
+    };
+  },
+  methods: {
+    getProdutos() {
+      fetch("http://localhost:3000/produto")
+        .then((r) => r.json())
+        .then((data) => {
+          this.produtos = data;
+        });
+    },
+  },
+  created() {
+    this.getProdutos();
+  },
+};
+</script>
+
+<style>
+</style>
